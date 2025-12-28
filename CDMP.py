c1ffb4e76de5bf7ef81d9016f26b00117eca4cc6
Arhaan Ali <aliarhaan761@gmail.com>
2025-12-28 14:37:26 +0530

    CDMP model test run

--- CDMP.py
+++ CDMP.py
@@ -1,9 +1,14 @@
 import torch
 import torchvision
+from torchvision import models
 import torchvision.transforms as transforms
+from torch.optim import lr_scheduler
 import torch.nn as nn
-from torch.utils.data import Dataset, DataLoader
+import os
+import time
+from torch.utils.data import  DataLoader
 import torch.optim as optim
+from tempfile import TemporaryDirectory
 
 transform = transforms.Compose(
     [transforms.ToTensor(),
@@ -12,16 +17,127 @@ transform = transforms.Compose(
 
 batch_size = 4
 
+dataloader = { }
+
 trainset = torchvision.datasets.ImageFolder(root='Finalised_Dataset/Train', transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+dataloader['Train'] = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
                                           shuffle=True, num_workers=0)
 
 testset = torchvision.datasets.ImageFolder(root='Finalised_Dataset/Test', transform=transform)
-testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+dataloader['Test'] = torch.utils.data.DataLoader(testset, batch_size=batch_size,
                                          shuffle=False, num_workers=0)
 
 validateset  = torchvision.datasets.ImageFolder(root='Finalised_Dataset/Validate', transform=transform)
-validateloader = torch.utils.data.DataLoader(validateset, batch_size=batch_size,
+dataloader['Validate'] = torch.utils.data.DataLoader(validateset, batch_size=batch_size,
                                          shuffle=False, num_workers=0)
 
+dataset_sizes = {"Train" : len(trainset) , "Test"  : len(testset) ,  "Validate" : len(validateset)}
+
+
+device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
+print(f"Using {device} device")
+
+def train_model(model, criterion , optimizer , scheduler , num_epoch = 1) :
+    since = time.time()
+
+
+    with TemporaryDirectory() as tempdir:
+        best_model_params_path = os.path.join(tempdir, 'best_model_params.pt')
+
+        torch.save(model.state_dict(), best_model_params_path)
+        best_acc = 0.0
+
+        for epoch in range(num_epoch) :
+            print(f"Epoch {epoch}/{num_epoch - 1}")
+            print('-' * 10)
+
+            for phase in ['Train' , 'Validate']:
+                if phase == 'Train' :
+                    model.train()
+
+                else:
+                    model.eval()
+
+                running_loss = 0.0
+                running_corrects = 0
+
+                for inputs, labels in dataloader[phase]:
+                    inputs = inputs.to(device)
+                    labels = labels.to(device)
+
+                    optimizer.zero_grad()
+
+                    with torch.set_grad_enabled(phase == 'Train'):
+                        outputs = model(inputs)
+                        _, preds = torch.max(outputs, 1)
+                        loss = criterion(outputs, labels)
+
+                        if phase == 'Train':
+                            loss.backward()
+                            optimizer.step()
+
+
+                    running_loss += loss.item() * inputs.size(0)
+                    running_corrects += torch.sum(preds == labels.data)
+                if phase == 'Train':
+                    scheduler.step()
+
+                epoch_loss = running_loss / dataset_sizes[phase]
+                epoch_acc = running_corrects.double() / dataset_sizes[phase]
+
+                print(f'{phase} Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}')
+
+
+                if phase == 'Validate' and epoch_acc > best_acc:
+                    best_acc = epoch_acc
+                    torch.save(model.state_dict(), best_model_params_path)
+
+            print()
+
+        time_elapsed = time.time() - since
+        print(f'Training complete in {time_elapsed // 60:.0f}m {time_elapsed % 60:.0f}s')
+        print(f'Best val Acc: {best_acc:4f}')
+
+
+        model.load_state_dict(torch.load(best_model_params_path, weights_only=True))
+    return model
+
+model_ft = models.resnet18(weights='IMAGENET1K_V1')
+num_ftrs = model_ft.fc.in_features
+# Here the size of each output sample is set to 2.
+# Alternatively, it can be generalized to ``nn.Linear(num_ftrs, len(class_names))``.
+model_ft.fc = nn.Linear(num_ftrs, 4)
+
+model_ft = model_ft.to(device)
+
+criterion = nn.CrossEntropyLoss()
+
+optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
+
+exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
+
+model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler,
+                       num_epoch = 1)
+
+model_conv = torchvision.models.resnet18(weights='IMAGENET1K_V1')
+for param in model_conv.parameters():
+    param.requires_grad = False
+
+# Parameters of newly constructed modules have requires_grad=True by default
+num_ftrs = model_conv.fc.in_features
+model_conv.fc = nn.Linear(num_ftrs, 4)
+
+model_conv = model_conv.to(device)
+
+criterion = nn.CrossEntropyLoss()
+
+# Observe that only parameters of final layer are being optimized as
+# opposed to before.
+optimizer_conv = optim.SGD(model_conv.fc.parameters(), lr=0.001, momentum=0.9)
+
+# Decay LR by a factor of 0.1 every 7 epochs
+exp_lr_scheduler = lr_scheduler.StepLR(optimizer_conv, step_size=7, gamma=0.1)
+
+model_conv = train_model(model_conv, criterion, optimizer_conv,
+                         exp_lr_scheduler, num_epoch = 1)
 
